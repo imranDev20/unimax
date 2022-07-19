933dd167e2fc8dbff42364749499e7d3eed5847e
imrandev20 <imrandev20@gmail.com>
2022-07-20 03:55:58 +0600

    added grid layout2

--- src/pages/team.js
+++ src/pages/team.js
@@ -42,7 +42,7 @@ const TeamPage = () => {
         </SectionText>
       </section>
 
-      <section className="container px-10 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-20">
+      <section className="container px-10 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-20">
         {team.map((member) => (
           <TeamCard
             key={member?.strapi_id}
